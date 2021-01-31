9925e313820f76bb9e323a42e86f6d4d55f03459
fraser148 <fjrennie1@outlook.com>
2021-01-31 02:21:20 +0000

    v1.0.2

--- scraper.py
+++ scraper.py
@@ -6,6 +6,9 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 
+preference = {"UPPER BOD":["Upper Reading Room Desk Booking","10:00","13:00","16:30"],
+"LOWER BOD":["Lower Reading Room Desk Booking","10:00","13:00","16:30"]}
+
 def hasXpath(xpath,driver):
     try:
         driver.find_element_by_xpath(xpath)
@@ -25,6 +28,7 @@ def highlight(element):
     apply_style(orignal_style)
 
 def book(userdata, service):
+    global preference
     driver = webdriver.Remote(service.service_url)
     driver.get('https://spacefinder.bodleian.ox.ac.uk/')
     time.sleep(1)
@@ -51,7 +55,7 @@ def book(userdata, service):
     okay = False
     while okay != True:
         try:
-            slot = driver.find_element_by_xpath("//div[contains(h5, 'Lower Reading Room Desk Booking') and contains(p, '13:00')]/parent::*/descendant::a")
+            slot = driver.find_element_by_xpath("//div[contains(h5, '"+preference[userdata[7]["morning"]][0]+"') and contains(p, '"+preference[userdata[7]["morning"]][1]+"')]/parent::*/descendant::a")
             #highlight(slot)
             slot.click()
             okay = True
@@ -61,14 +65,25 @@ def book(userdata, service):
                 print("NO MORE SLOTS")
             else:
                 print("Slots are loading")
-    time.sleep(2)
-    confirm = driver.find_element_by_name("ctl00$ContentPlaceHolder$Cart$CheckoutButton")
-    confirm.click()
-    time.sleep(3)
-    for key in userdata[0]:
-        element = driver.find_element_by_id(key)
-        element.send_keys(userdata[0][key])
-
+    okay = False
+    while okay != True:
+        try:
+            confirm = driver.find_element_by_name("ctl00$ContentPlaceHolder$Cart$CheckoutButton")
+            confirm.click()
+            okay = True
+        except:
+            print("Loading!")
+    okay = False
+    while okay != True:
+        try:
+            for key in userdata[0]:
+                element = driver.find_element_by_id(key)
+                element.send_keys(userdata[0][key])
+            confirm = driver.find_element_by_name("UPDATE ME")
+            confirm.click()
+            okay = True
+        except:
+            print("Loading Form!")
     time.sleep(20)
     
 
@@ -76,13 +91,20 @@ ids = ["FirstNameundefined","LastNameundefined","Phoneundefined","Street2undefin
 
 # Gets data from json file and returns a dictionary of lists for user data
 userdata = {}
-
-with open('data.json') as json_file:
-    data = json.load(json_file)
-    for p in data['users']:
-        current_user = p
-        temp = []
-        userdata[current_user] = [data['users'][p][item] for item in data['users'][p]]
+try:
+    with open('data.json') as json_file:
+        data = json.load(json_file)
+        for p in data['users']:
+            current_user = p
+            temp = []
+            userdata[current_user] = [data['users'][p][item] for item in data['users'][p]]
+except:
+    with open('userdata.json') as json_file:
+        data = json.load(json_file)
+        for p in data['users']:
+            current_user = p
+            temp = []
+            userdata[current_user] = [data['users'][p][item] for item in data['users'][p]]
 
 userkeys = []
 index = 0
@@ -105,45 +127,3 @@ for user in userkeys:
     t = threading.Thread(target=book, args=(user,service))
     threads.append(t)
     t.start()
-
-# #for _ in range(0,len(userkeys)):
-# driver = webdriver.Remote(service.service_url)
-# #for driver in drivers:
-# driver.get('https://spacefinder.bodleian.ox.ac.uk/')
-# time.sleep(1)
-
-# email = driver.find_element_by_id('i0116')
-# email.send_keys(email_key)
-# submit = driver.find_element_by_id("idSIButton9")
-# submit.click()
-# time.sleep(1)
-# password = driver.find_element_by_id("i0118")
-# password.send_keys(pwd)
-# time.sleep(1)
-# signin = driver.find_element_by_id("idSIButton9")
-# signin.click()
-# time.sleep(3)
-# calendar = driver.find_element_by_xpath("//span[@aria-label='" + day + "']")
-# calendar.click()
-# time.sleep(2)
-# slot = driver.find_element_by_xpath("//div[contains(h5, 'Lower Reading Room Desk Booking') and contains(p, '10:00')]/parent::*/descendant::a")
-# #highlight(slot)
-# slot.click()
-# time.sleep(2)
-# confirm = driver.find_element_by_name("ctl00$ContentPlaceHolder$Cart$CheckoutButton")
-# confirm.click()
-# time.sleep(3)
-
-# #ids = {"FirstNameundefined":"Fraser","LastNameundefined":"Rennie","Phoneundefined":"1266833","Street2undefined":"Exeter","Emailundefined":"fraser.rennie@exeter.ox.ac.uk","ConfirmEmailundefined":"fraser.rennie@exeter.ox.ac.uk"}
-
-# element = driver.find_element_by_xpath("//option[@label='Mr']")
-# element.click()
-
-
-
-# for key in ids:
-#     element = driver.find_element_by_id(key)
-#     element.send_keys(ids[key])
-
-# time.sleep(10)
-# driver.close()
